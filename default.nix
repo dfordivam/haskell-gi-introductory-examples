0733357bf3993419b13648d7f1aed3c71bbe3dc2
Divam <dfordivam@gmail.com>
2018-07-06 19:41:14 +0900

    Add some overrides

--- default.nix
+++ default.nix
@@ -6,6 +6,12 @@ let
       ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
       ghcWithPackages = self.ghc.withPackages;
       ListLike = super.callHackage "ListLike" "4.6" {};
+      hoogle = super.callHackage "hoogle" "5.0.17.2" {};
+      conduit = super.callHackage "conduit" "1.3.0.1" {};
+      conduit-extra = super.callHackage "conduit-extra" "1.3.0"{};
+      xml-conduit = super.callHackage "xml-conduit" "1.8.0"{};
+      resourcet = super.callHackage "resourcet" "1.2.0" {};
+      http-conduit = super.callHackage "http-conduit" "2.3.0" {};
     };
   };
 
